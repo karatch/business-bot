9f2b07fae0afd7fa20255aa4c0d04776b76f6ff3
Alexey Karachentsev <alexey.karachentsev@vniira.spb.su>
2025-12-08 10:34:22 +0300

    make class Settings to store the bot token

---
+++ bot/config.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+@dataclass
+class Settings:
+    bot_token: str
+    admin_chat_id: int
+
+
+def get_settings() -> Settings:
+    settings = Settings(
+        bot_token = os.getenv('BOT_TOKEN'),
+        admin_chat_id = int(os.getenv('ADMIN_CHAT_ID'))
+    )
+    return settings
+
+settings = get_settings()
+print(settings)
